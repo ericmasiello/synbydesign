b48dd46520e27827050dac554192332b05e07c9d
ericmasiello <eric.j.masiello@gmail.com>
2016-01-16 16:20:54 -0500

    Made a basic LoadingStatus component that is then loaded into my Container LoadingStatus component
    Created very basic test for LoadingStatus component

---
+++ app/components/LoadingStatus.spec.js
@@ -0,0 +1,39 @@
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+import TestUtils from 'react-addons-test-utils';
+import React from 'react';
+import LoadingStatus from './../components/LoadingStatus';
+
+describe('LoadingStatus', () => {
+
+  it('should not have a progress property by default', () => {
+
+    const appLoadingProp = {
+      activeRequests: 0,
+      loadedRequests: 0
+    };
+
+    const r = TestUtils.createRenderer();
+    r.render(<LoadingStatus appLoading={appLoadingProp} />);
+    const actual = r.getRenderOutput();
+    expect(actual.progress).toNotExist();
+  });
+
+  it('should render an empty div by default', () => {
+
+    const appLoadingProp = {
+      activeRequests: 0,
+      loadedRequests: 0
+    };
+
+    const r = TestUtils.createRenderer();
+    r.render(<LoadingStatus appLoading={appLoadingProp} />);
+    const actual = r.getRenderOutput();
+    const expected = (
+      <div></div>
+    );
+
+    expect(actual).toIncludeJSX(expected);
+  })
+});
