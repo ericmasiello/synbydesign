c7369777f4508b4132edda252d7a8adf953bc367
ericmasiello <eric.j.masiello@gmail.com>
2016-01-17 09:57:09 -0500

    Add tests for reducers

---
+++ app/reducers/portfolio.reducer.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import reducer from './portfolio.reducer';
+import { LOAD_PORTFOLIO_ALL, LOAD_PORTFOLIO_DETAIL } from '../actions/types';
+
+describe('Portfolio Reducer', () => {
+
+  it('should return payload data when responding to LOAD_PORTFOLIO_ALL', ()=>{
+
+    const actual = reducer([], {
+      type: LOAD_PORTFOLIO_ALL,
+      payload: {
+        data: ['first', 'second', 'third']
+      }
+    });
+
+    const expected = ['first', 'second', 'third'];
+
+    expect(actual).toEqual(expected);
+  });
+});
