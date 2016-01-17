fb6c62a115672351b59936e2459bd29a33d35865
ericmasiello <eric.j.masiello@gmail.com>
2016-01-17 12:04:47 -0500

    Add mocking data for testing Add unit test for portfolio-list component

---
+++ app/components/portfolio-list.component.spec.js
@@ -0,0 +1,86 @@
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+import TestUtils from 'react-addons-test-utils';
+import React from 'react';
+import PortfolioList from './../components/portfolio-list.component';
+import mockWebPortfolio from '../test-utils/portfolio-web.mock.json';
+import mockDesignPortfolio from '../test-utils/portfolio-design.mock.json';
+import mockOtherPortfolio from '../test-utils/portfolio-other.mock.json';
+
+describe('PortfolioList', () => {
+
+  const mockLoadAllPortfolio = function(){};
+  const WebTitle = <h1 className="h3  text-center">Web Design &amp; Development</h1>;
+  const DesignTitle = <h1 className="h3  text-center">Illustration, Logos, &amp; Flyers</h1>;
+  const OtherTitle = <h1 className="h3  text-center">Other Rad Stuff</h1>;
+
+  it('should render its root element as a section', () => {
+
+    const r = TestUtils.createRenderer();
+    r.render(<PortfolioList loadAllPortfolio={mockLoadAllPortfolio} portfolio={[]} />);
+    const actual = r.getRenderOutput();
+    const rootTag = actual.type;
+    const expected = 'section';
+
+    expect(rootTag).toEqual(expected);
+  });
+
+  it('should request portfolio items of the appropriate categories', () => {
+
+    let actual = [];
+    const customMockLoadAllPortfolio = (categories) => {
+      actual = categories
+    };
+
+    const r = TestUtils.createRenderer();
+    r.render(<PortfolioList loadAllPortfolio={customMockLoadAllPortfolio} portfolio={[]} />);
+    r.getRenderOutput();
+    const expected = ['web','other', 'logos', 'illustration', 'flyers', 'business-cards'];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should not render web, design, other headings when no corresponding portfolio data is provided', () => {
+
+    const r = TestUtils.createRenderer();
+    r.render(<PortfolioList loadAllPortfolio={mockLoadAllPortfolio} portfolio={[]} />);
+    const actual = r.getRenderOutput();
+    expect(actual).toNotIncludeJSX(WebTitle);
+    expect(actual).toNotIncludeJSX(DesignTitle);
+    expect(actual).toNotIncludeJSX(OtherTitle);
+  });
+
+  it('should render the web section when web portfolio items are provided', () => {
+
+    const r = TestUtils.createRenderer();
+    r.render(<PortfolioList loadAllPortfolio={mockLoadAllPortfolio} portfolio={[...mockWebPortfolio]} />);
+    const actual = r.getRenderOutput();
+
+    expect(actual).toIncludeJSX(WebTitle);
+    expect(actual).toNotIncludeJSX(DesignTitle);
+    expect(actual).toNotIncludeJSX(OtherTitle);
+  });
+
+  it('should render the design section when design portfolio items are provided', () => {
+
+    const r = TestUtils.createRenderer();
+    r.render(<PortfolioList loadAllPortfolio={mockLoadAllPortfolio} portfolio={[...mockDesignPortfolio]} />);
+    const actual = r.getRenderOutput();
+
+    expect(actual).toNotIncludeJSX(WebTitle);
+    expect(actual).toIncludeJSX(DesignTitle);
+    expect(actual).toNotIncludeJSX(OtherTitle);
+  });
+
+  it('should render the other section when other portfolio items are provided', () => {
+
+    const r = TestUtils.createRenderer();
+    r.render(<PortfolioList loadAllPortfolio={mockLoadAllPortfolio} portfolio={[...mockOtherPortfolio]} />);
+    const actual = r.getRenderOutput();
+
+    expect(actual).toNotIncludeJSX(WebTitle);
+    expect(actual).toNotIncludeJSX(DesignTitle);
+    expect(actual).toIncludeJSX(OtherTitle);
+  });
+});
