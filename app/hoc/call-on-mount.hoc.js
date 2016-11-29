73b141b9e4fe724d5544f9f9d9cf41f9a23ba063
Eric Masiello <eric.j.masiello@gmail.com>
2016-11-29 06:46:26 -0500

    Remove only

--- app/hoc/call-on-mount.hoc.js
+++ app/hoc/call-on-mount.hoc.js
@@ -1,31 +1,30 @@
-'use strict';
 import React, { Component } from 'react';
 
-export default function callOnMountHOC() {
-
-  const args = [...arguments];
+export default function callOnMountHOC(...rest) {
+  const args = rest;
   let testIfIShouldRunOnMount = () => true;
   let runOnMount = () => true;
 
-  if(args.length === 1) {
+  if (args.length === 1) {
     runOnMount = args[0];
-  } else if( args.length === 2){
+  } else if (args.length === 2) {
     testIfIShouldRunOnMount = args[0];
     runOnMount = args[1];
   }
 
-  return function(WrappedComponent) {
-
+  return function callOnMountHOCInner(WrappedComponent) {
     const isClassComponent = Object.getPrototypeOf(WrappedComponent) === Component;
-    WrappedComponent = !isClassComponent ? Component : WrappedComponent;
+    const RenderWrappedComponent = !isClassComponent ? Component : WrappedComponent;
 
-    return class CallOnMountEnhancer extends WrappedComponent {
+    return class CallOnMountEnhancer extends RenderWrappedComponent {
       componentWillMount() {
-        testIfIShouldRunOnMount.call(this) && runOnMount.call(this);
+        if (testIfIShouldRunOnMount.call(this)) {
+          runOnMount.call(this);
+        }
       }
 
       render() {
-        return isClassComponent ? super.render() : <WrappedComponent />;
+        return isClassComponent ? super.render() : <RenderWrappedComponent />;
       }
     };
   };
