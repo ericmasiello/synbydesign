b3d1a48fa4d5d8b54ea62b5008ace3db9d651c89
Eric Masiello <eric.j.masiello@gmail.com>
2016-11-29 22:35:24 -0500

    Add semi-working configuration for building using webpack. Still needs some work.

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+/* eslint-disable comma-dangle */
+const path = require('path');
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+/* eslint-enable import/no-extraneous-dependencies */
+const parts = require('./webpack/parts');
+
+const TARGET = process.env.npm_lifecycle_event || 'dev';
+const PATHS = {
+  build: path.join(__dirname, 'build/'),
+  script: path.join(__dirname, 'app'),
+};
+
+const common = merge(
+  {
+    entry: {
+      app: PATHS.script
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Syn By Design - Eric Masiello\'s Portfolio',
+        googleAnalyticsCode: 'UA-162757-4',
+        template: 'app/index.tpl.html',
+        hash: true,
+        minify: {
+          removeComments: true,
+          removeCommentsFromCDATA: true,
+          collapseWhitespace: true,
+          removeEmptyAttributes: true
+        }
+      })
+    ],
+    externals: {
+      cheerio: 'window',
+      'react/addons': true,
+      'react/lib/ExecutionEnvironment': true,
+      'react/lib/ReactContext': true
+    },
+  },
+  parts.setupBabel([PATHS.script]),
+  parts.setupCSS()
+);
+
+let config;
+
+switch (TARGET) {
+  case 'build':
+    config = merge(
+      common,
+      parts.setupSourceMaps('build'),
+      parts.configureEnvironment('production')
+    );
+    break;
+  default:
+    config = merge(
+      common,
+      parts.setupSourceMaps(),
+      // parts.setupESLint([PATHS.script]),
+      parts.devServer(PATHS.build, {
+        proxy: [
+          {
+            path: '/wp/wp-json*',
+            target: 'http://localhost/synbydesign'
+          }
+        ]
+      }),
+      parts.configureEnvironment('development')
+    );
+}
+
+module.exports = validate(config);
---
+++ webpack/parts.js
@@ -0,0 +1,104 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+/* eslint-enable import/no-extraneous-dependencies */
+
+exports.setupSourceMaps = (type = 'dev') => {
+  switch (type) {
+    case 'build':
+      return {
+        devtool: 'source-map'
+      };
+    default:
+      return {
+        devtool: 'eval-source-map'
+      };
+  }
+};
+
+exports.configureEnvironment = env => ({
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(env) // weird hack
+      }
+    })
+  ]
+});
+
+exports.devServer = (path, customDevServerConfig) => ({
+  devServer: Object.assign({}, {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    stats: 'errors-only',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    outputPath: path
+  }, customDevServerConfig),
+  plugins: [
+    new webpack.HotModuleReplacementPlugin({
+      multiStep: true
+    })
+  ]
+});
+
+exports.setupESLint = paths => ({
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint',
+        include: paths
+      }
+    ]
+  }
+});
+
+exports.setupBabel = paths => ({
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true
+        },
+        include: paths
+      }
+    ]
+  }
+});
+
+exports.setupIcons = paths => ({
+  module: {
+    loaders: [
+      {
+        test: /\.svg$/,
+        loaders: ['svg-inline'],
+        include: paths
+      }
+    ]
+  }
+});
+
+exports.setupCSS = paths => ({
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: paths
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass'],
+        include: paths
+      }
+    ]
+  },
+  postcss: () => [autoprefixer({
+    browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'safari >= 4']
+  })]
+});
