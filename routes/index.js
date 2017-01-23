312cd0665f50b11a5ef1731c533b0a960971a424
Eric Masiello <eric.j.masiello@gmail.com>
2017-01-23 07:38:44 -0500

    Fix linting errors

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ const request = require('request');
 
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-    console.log('Root route');
-    request.get('https://dl.dropboxusercontent.com/u/4825781/synbydesign.json', function(err, resp, body){
-        res.send(JSON.parse(body));
-    });
+router.get('/', (req, res) => {
+  request.get('https://dl.dropboxusercontent.com/u/4825781/synbydesign.json', (err, resp, body) => {
+    res.send(JSON.parse(body));
+  });
 });
 
 module.exports = router;
