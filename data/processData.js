7a9e816842de95d5d5fc659047cf5ba6a4529869
Eric Masiello <eric.j.masiello@gmail.com>
2017-01-30 09:57:47 -0500

    Better data processing

--- data/processData.js
+++ data/processData.js
@@ -1,32 +1,61 @@
 const fs = require('fs');
 const stub = require('./stub.json');
 const cheerio = require('cheerio');
+const _ = require('lodash');
+
+const buildSizes = (featured_image) => {
+  const { attachment_meta, source } = featured_image;
+  if (!attachment_meta) {
+    return {};
+  }
+
+  const sizes = _.reduce(attachment_meta.sizes, (acc, size, key) => {
+    if(key === 'post-thumbnail') {
+      return acc;
+    }
+    return Object.assign({}, acc, {
+      [key]: {
+        url: size.url,
+        width: size.width,
+        height: size.height,
+      }
+    });
+  }, {
+    full: {
+      url: source,
+      width: attachment_meta.width,
+      height: attachment_meta.height,
+    }
+  });
+
+  return sizes;
+};
 
 const result = stub.map(({
   title,
   custom_meta,
-  // featured_image,
   content,
-  source,
   terms,
+  featured_image,
 }) => {
   const $ = cheerio.load(content);
   const $img = $('img').attr('class', '');
+  const description = $img.attr('alt');
   const { mix_url, stack_design, svg, track_list, website_url } = custom_meta;
   return ({
     title,
-    // featured_image,
+    description,
     meta: {
-      stack_design,
-      mix_url,
-      track_list,
-      website_url,
-      is_svg: !!svg,
+      stackDesign: stack_design,
+      mixUrl: mix_url,
+      trackList: track_list,
+      websiteUrl: website_url,
+      isSVG: !!svg,
     },
-    image: svg || $img.wrap('<div></div>').parent().html(),
-    source,
     category: terms.category.map(category => category.slug),
     tags: Array.isArray(terms.post_tag) ? terms.post_tag.map(tag => tag.name) : [],
+    svgSource: svg || null,
+    imagePaths: buildSizes(featured_image),
   });
 });
 
