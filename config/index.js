cc7bf2cbc92f7763b60aa2dcd55a217907d14f76
Eric Masiello <emasiello@vistaprint.com>
2017-01-31 10:34:27 -0500

    Add winston logging

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
 /* @flow */
 require('dotenv').config();
+const winston = require('winston');
+
+// TODO: Add this to .env
+winston.level = process.env.winston || 'debug';
 
 export const MORGAN_CONFIG: string = process.env.morgan || '';
 export const PORT: number = parseInt(process.env.PORT, 10) || 3000;
--- services/portfolio.js
+++ services/portfolio.js
@@ -1,14 +1,21 @@
 /* @flow */
 import req from 'request';
 import Promise from 'bluebird';
+import winston from 'winston';
 import { SYN_BY_DESIGN_ROUTE } from '../config';
 
 const request = Promise.promisify(req);
 
 function fetchAll() {
-  return request(`${SYN_BY_DESIGN_ROUTE}/portfolio.json`)
-    .then(({ body }) => JSON.parse(body))
+  const url = `${SYN_BY_DESIGN_ROUTE}/portfolio.json`;
+  winston.info(`Requesting ${url}`);
+  return request(url)
+    .then(({ body }) => {
+      winston.info(`Response from ${url}`, body);
+      return JSON.parse(body);
+    })
     .catch((err) => {
+      winston.error(err);
       throw new Error(err);
     });
 }
