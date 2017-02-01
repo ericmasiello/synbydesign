6430b0234d53cf8bb3cc86e917aa4f4ef8d8e593
Eric Masiello <eric.j.masiello@gmail.com>
2017-02-01 07:03:19 -0500

    fix linting

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,10 @@
 /* @flow */
 // FIXME: Should only do this in dev env
 require('../config/babel');
+const winston = require('winston');
 const app = require('../server');
 const PORT = require('../config').PORT;
 
 app.set('port', PORT);
 
-app.listen(app.get('port'), () => console.log(`Server started on port ${app.get('port').toString()}`));
+app.listen(app.get('port'), () => winston.info(`Server started on port ${app.get('port').toString()}`));
