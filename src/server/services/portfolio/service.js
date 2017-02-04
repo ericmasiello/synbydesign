f50bc77e8fd7d8720c450560fc0282c89f440512
Eric Masiello <eric.j.masiello@gmail.com>
2017-02-04 18:16:00 -0500

    Remove excess logging

--- src/server/services/portfolio/service.js
+++ src/server/services/portfolio/service.js
@@ -12,10 +12,7 @@ service.fetchAll = function fetchAll() {
   const url = `${SYN_BY_DESIGN_ROUTE}/portfolio.json`;
   winston.info(`Requesting ${url}`);
   return request(url)
-    .then(({ body }) => {
-      winston.info(`Response from ${url}`, body);
-      return JSON.parse(body);
-    })
+    .then(({ body }) => JSON.parse(body))
     .catch((err) => {
       winston.error(err);
       throw new Error(err);
