9cf3f75e1ebd9ef77b852faad616eed938fc417f
Eric Masiello <eric.j.masiello@gmail.com>
2017-02-26 19:39:13 -0500

    Add test for handling errors in about saga

--- test/unit/app/About/sagas.test.js
+++ test/unit/app/About/sagas.test.js
@@ -1,14 +1,23 @@
 import { expect } from 'chai';
-import { call, put } from 'redux-saga/effects';
-import { fetchAbout, Api } from '../../../../src/app/About/sagas';
-import { LOAD_ABOUT_SUCCEEDED } from '../../../../src/app/About/actions';
+import {
+  call,
+  put,
+} from 'redux-saga/effects';
+import {
+  fetchAbout,
+  Api,
+} from '../../../../src/app/About/sagas';
+import {
+  LOAD_ABOUT_SUCCEEDED,
+  LOAD_ABOUT_FAILED,
+} from '../../../../src/app/About/actions';
 
-describe.only('About Sagas', function () {
+describe('About Sagas', function () {
   it('should work', function () {
     expect(true).to.equal(true);
   });
 
-  it('should fetch about', () => {
+  it('should fetch about content', function () {
     const generator = fetchAbout();
     expect(generator.next().value).to.deep.equal(call(Api.fetchAbout));
     const aboutResponse = {
@@ -18,5 +27,15 @@ describe.only('About Sagas', function () {
       type: LOAD_ABOUT_SUCCEEDED,
       payload: 'Hello world',
     }));
+  });
+
+  it('should fail to fetch about content', function () {
+    const generator = fetchAbout();
+    expect(generator.next().value).to.deep.equal(call(Api.fetchAbout));
+    const error = new Error('An error occurred');
+    expect(generator.throw(error).value).to.deep.equal(put({
+      type: LOAD_ABOUT_FAILED,
+      message: 'An error occurred',
+    }))
   });
 });
