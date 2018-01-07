e1c509bda032040990904bb87a86fd25fcae642a
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-07 09:55:30 -0500

    Get background image rendering in header

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -6,27 +6,21 @@ import Nav from './Nav';
 import { pxToRem } from '../styles/utils';
 import { maxWidth, horizontalPadding } from '../styles/vars';
 
-interface Props {
-  className?: string;
-}
+const HeaderBackgroundImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1;
+  filter: grayscale(.85);
+  transform: translateY(-50%);
+`;
 
-export const Header: React.SFC<Props> = ({ className }) => {
-  return (
-    <header className={className}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <Nav />
-    </header>
-  );
-};
+HeaderBackgroundImage.displayName = 'Header.BackgroundImage';
 
-Header.displayName = 'Header';
-
-export default styled(Header)`
-  padding: 1rem ${pxToRem(horizontalPadding)};
-  max-width: ${pxToRem(maxWidth)};
-  margin: auto;
+const HeaderContent = styled.div`
+  position: relative;
+  z-index: 2;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -35,4 +29,36 @@ export default styled(Header)`
     flex-direction: row;
     justify-content: space-between;
   }
+`;
+
+HeaderContent.displayName = 'Header.Content';
+
+
+interface Props {
+  className?: string;
+  imagePath?: string;
+}
+
+export const Header: React.SFC<Props> = ({ className, imagePath }) => {
+  return (
+    <header className={className}>
+      {imagePath && <HeaderBackgroundImage src={imagePath} alt="" />}
+      <HeaderContent>
+        <Link to="/">
+          <Logo />
+        </Link>
+        <Nav />
+      </HeaderContent>
+    </header>
+  );
+};
+
+Header.displayName = 'Header';
+
+export default styled(Header)`
+  position: relative;
+  padding: 1rem ${pxToRem(horizontalPadding)};
+  max-width: ${pxToRem(maxWidth)};
+  margin: auto;
+  overflow: hidden;
 `;
