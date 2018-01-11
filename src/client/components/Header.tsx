fbf655160205056416f96f69be35cbfe2ab1d7ad
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-11 08:22:12 -0500

    Refactor Header to support BG images

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -6,23 +6,27 @@ import Nav from './Nav';
 import { pxToRem } from '../styles/utils';
 import { maxWidth, horizontalPadding } from '../styles/vars';
 
-const HeaderBackgroundImage = styled.img`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1;
-`;
+const showBGImageMediaQuery = pxToRem(500);
 
-HeaderBackgroundImage.displayName = 'Header.BackgroundImage';
+interface HeaderContentProps {
+  className?: string;
+  highlight?: boolean;
+}
 
-const HeaderContent = styled.div`
+const HeaderContent: React.SFC<HeaderContentProps> = props => (
+  <div className={props.className}>
+    {props.children}
+  </div>
+);
+
+const StyledHeaderContent = styled(HeaderContent)`
   position: relative;
   z-index: 2;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 1rem ${pxToRem(horizontalPadding)};
+  background-color: ${props => props.highlight ? 'rgba(255, 255, 255, 0.65)' : 'transparent'};
 
   @media(min-width: ${pxToRem(850)}) {
     flex-direction: row;
@@ -33,30 +37,23 @@ const HeaderContent = styled.div`
 HeaderContent.displayName = 'Header.Content';
 
 
-interface Props {
+interface HeaderProps {
   className?: string;
   imagePath?: string;
 }
 
-export const Header: React.SFC<Props> = ({ className, imagePath }) => {
+export const Header: React.SFC<HeaderProps> = ({ className, imagePath }) => {
   return (
     <header
       className={className}
-      style={{
-        minHeight: imagePath ? pxToRem(600) : 0,
-      }}
     >
-      {imagePath && <HeaderBackgroundImage src={imagePath} alt="" />}
-      <HeaderContent
-        style={{
-          backgroundColor: imagePath ? 'rgba(255, 255, 255, 0.65)' : 'transparent',
-        }}
-      >
+      <StyledHeaderContent highlight={!!imagePath}>
         <Link to="/">
           <Logo />
         </Link>
         <Nav />
-      </HeaderContent>
+      </StyledHeaderContent>
+      <div className="bg" />
     </header>
   );
 };
@@ -67,5 +64,22 @@ export default styled(Header)`
   position: relative;
   max-width: ${pxToRem(maxWidth)};
   margin: auto;
-  overflow: hidden;
+  min-height: ${(props => props.imagePath ? pxToRem(600) : '0')};
+
+  .bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    ${/* TODO: only apply filter when you want the header in certain cases */ ``}
+    ${/* filter: blur(3px) grayscale(40%); */ ``}
+
+    @media (min-width: ${showBGImageMediaQuery}) {
+      background-image: ${props => props.imagePath ? `url('${props.imagePath}')` : 'none'};
+      background-size: cover;
+      background-position: 50%;
+    }
+  }
 `;
