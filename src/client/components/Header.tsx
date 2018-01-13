66e0fc921e7063ab9241e3d2d478d59d0584d3d9
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-13 08:33:08 -0500

    Refactor Header into two components

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -3,59 +3,29 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from './Logo';
 import Nav from './Nav';
-import { pxToRem } from '../styles/utils';
-import { maxWidth, horizontalPadding } from '../styles/vars';
+import HeaderContent from './HeaderContent';
 
-interface HeaderContentProps {
+interface Props {
   className?: string;
 }
 
-const HeaderContent: React.SFC<HeaderContentProps> = props => (
-  <div className={props.className}>
-    {props.children}
-  </div>
-);
-
-const StyledHeaderContent = styled(HeaderContent)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem ${pxToRem(horizontalPadding)};
-  background-color: rgba(255, 255, 255, 0.90);
-
-  @media(min-width: ${pxToRem(850)}) {
-    flex-direction: row;
-    justify-content: space-between;
-  }
-`;
-
-HeaderContent.displayName = 'Header.Content';
-
-
-interface HeaderProps {
-  className?: string;
-}
-
-export const Header: React.SFC<HeaderProps> = ({ className }) => {
+export const Header: React.SFC<Props> = ({ className }) => {
   return (
     <header
       className={className}
     >
-      <StyledHeaderContent>
+      <HeaderContent>
         <Link to="/">
           <Logo />
         </Link>
         <Nav />
-      </StyledHeaderContent>
+      </HeaderContent>
     </header>
   );
 };
 
 Header.displayName = 'Header';
 
-// TODO: can HeaderContent and Header be one thing now?
-
 export default styled(Header)`
-  max-width: ${pxToRem(maxWidth)};
-  margin: auto;
+  background-color: rgba(255, 255, 255, 0.90);
 `;
