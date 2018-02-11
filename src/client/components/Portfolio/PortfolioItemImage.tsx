c1dc7791df7fd95480005de93c2ce8de4c9f77b2
Eric Masiello <eric.j.masiello@gmail.com>
2018-02-11 16:03:23 -0500

    Use img tags intead of bg images

--- src/client/components/Portfolio/PortfolioItemImage.tsx
+++ src/client/components/Portfolio/PortfolioItemImage.tsx
@@ -7,7 +7,7 @@ interface Props {
   featured?: boolean;
   size?: string;
   position?: string;
-  tag?: Tag;
+  tag?: Tag; // remove me?
 }
 
 interface DefaultProps {
@@ -16,7 +16,9 @@ interface DefaultProps {
 
 const PortfolioItemImage: React.SFC<Props> = (props) => {
   const { tag: Tag, imagePath, featured, size, position, ...rest } = props as Props & DefaultProps;
-  return <Tag {...rest}/>;
+  return (
+    <img src={imagePath} alt="FIXME" {...rest} />
+  );
 };
 
 PortfolioItemImage.defaultProps = {
@@ -25,35 +27,41 @@ PortfolioItemImage.defaultProps = {
 
 PortfolioItemImage.displayName = 'PortfolioItemImage';
 
-const StyledPortfoioItemImage = styled(PortfolioItemImage)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: hidden;
-  background-image: ${props => `url('${props.imagePath}')`};
-  background-position: ${props => !props.featured ? `${props.position}` : 'center'};
-  width: 100%;
-  height: 100%;
-  background-size: ${(props) => {
-    if (props.size) {
-      return props.size;
-    }
-    return props.featured ? 'cover' : '90%';
-  }};
-  filter: ${props => !props.featured ? `grayscale(0.85)` : 'grayscale(0.65)'};
-  transition: filter 0.2s, transform 0.2s;
-  ${props => props.featured ? 'transform: scale(1.05);' : ''};
-
-  &:hover {
-    filter: grayscale(0);
-    transform: scale(1);
+export default styled(PortfolioItemImage)`
+  img {
+    object-fit: cover;
   }
 `;
 
-StyledPortfoioItemImage.defaultProps = {
-  position: '-20% -30%',
-};
+// const StyledPortfoioItemImage = styled(PortfolioItemImage)`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+//   overflow: hidden;
+//   background-image: ${props => `url('${props.imagePath}')`};
+//   background-position: ${props => !props.featured ? `${props.position}` : 'center'};
+//   width: 100%;
+//   height: 100%;
+//   background-size: ${(props) => {
+//     if (props.size) {
+//       return props.size;
+//     }
+//     return props.featured ? 'cover' : '90%';
+//   }};
+//   filter: ${props => !props.featured ? `grayscale(0.85)` : 'grayscale(0.65)'};
+//   transition: filter 0.2s, transform 0.2s;
+//   ${props => props.featured ? 'transform: scale(1.05);' : ''};
 
-export default StyledPortfoioItemImage;
+//   &:hover {
+//     filter: grayscale(0);
+//     transform: scale(1);
+//   }
+// `;
+
+// StyledPortfoioItemImage.defaultProps = {
+//   position: '-20% -30%',
+// };
+//
+// export default StyledPortfoioItemImage;
