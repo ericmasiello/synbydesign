8b9c8846f155e2b480bd40b42de650b1540118a0
Eric Masiello <eric.j.masiello@gmail.com>
2018-02-19 15:57:10 -0500

    Add skills and professional experience to resume data

--- src/server/data/resume.ts
+++ src/server/data/resume.ts
@@ -5,8 +5,94 @@ const resume: Resume = {
     Over 10 years of professional experience in web development and design
     with a passion for developing rich internet applications using modern
     JavaScript frameworks`,
-  skills: [],
-  professionalExperience: [],
+  skills: [
+    'Web application development',
+    'Responsive web design',
+    'Single page applications',
+    'Node',
+    'Express JS',
+    'HTML5',
+    'CSS',
+    'SCSS',
+    'Styled Components',
+    'React',
+    'React Native',
+    'Redux',
+    'ES2015/16',
+    'Jest',
+    'Mocha',
+    'Enzyme',
+    'Angular',
+    'jQuery',
+    'Webpack',
+    'Gulp',
+    'Babel',
+    'Wordpress',
+    'PHP',
+    'Git',
+    'Cross browser development',
+    'MySQL',
+    'Postgres',
+    'Mongo',
+  ],
+  professionalExperience: [{
+    organization: 'Vistaprint Digital',
+    roles: [{
+      title: 'Senior Software Engineer',
+      yearFrom: '2016',
+    }],
+    accomplishments: [
+      'Vistaprint Digital UI Component library architect',
+      'Full stack engineer across multiple products utilizing React and Node JS running on Heroku and AWS',
+      'Mentor junior & senior engineers on front-end and Node development',
+    ],
+  }, {
+    organization: 'Advisory Board Company',
+    roles: [{
+      title: 'Principal Front-End Developer',
+      yearFrom: '2013',
+      yearTo: '2016',
+    }, {
+      title: 'Lead Front-End Developer',
+      yearFrom: '2011',
+      yearTo: '2013',
+    }],
+    accomplishments: [
+      [
+        'Principal engineer on multiple health care and higher education RESTful',
+        'web applications using SCSS, Angular, Ionic, Cordova, Knockout, jQuery, Gulp, and Grunt',
+      ].join(' '),
+      'Advisory Board UI framework CSS Architect',
+      'Authored and trained two one–week-long front end developer boot camps',
+      'Coached and mentored junior engineers and designers',
+      'Winner of multiple Advisory Board Hackathon competitions',
+      'Acted as cross-product accessibility consultant',
+    ],
+  }, {
+    organization: 'US Census Bureau',
+    roles: [{
+      title: 'Chief of Human Resources Info Systems Branch',
+      yearFrom: '2011',
+      yearTo: '2010',
+    }, {
+      title: 'Senior Web Developer & Team Lead',
+      yearFrom: '2008',
+      yearTo: '2010',
+    }, {
+      title: 'Web Designer & Developer',
+      yearFrom: '2003',
+      yearTo: '2009',
+    }],
+    accomplishments: [
+      'Managed 9 direct reports',
+      'Acted as project manager for HR Systems: developing project plans, wireframes, & milestones',
+      [
+        'Developed and documented visual style guide for engineers,',
+        'enforcing HTML semantics, best practices, UI, and UX consistency',
+      ].join(' '),
+      'Lead developer for Census Human Resource Information System (CHRIS) using on .NET and SQL Server',
+    ],
+  }],
   freelanceExperience: [],
   education: [],
 };
