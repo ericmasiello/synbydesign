cc7e33bb9d31acfe306685fd9d5ee798e141aaca
Eric Masiello <eric.j.masiello@gmail.com>
2018-03-22 09:03:57 -0400

    Style the resume header

--- src/client/components/ResumeHeader.tsx
+++ src/client/components/ResumeHeader.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { type, scaleToRem } from '../styles/utils';
+import { COLORS, TYPE_SIZE } from '../styles/vars';
 
 interface Props {
   name: string;
@@ -18,9 +20,9 @@ export const ResumeHeader: React.SFC<Props> = props => {
     DefaultProps;
   return (
     <Tag className={className} {...rest}>
-      <h1>{name}</h1>
-      <h2>{title}</h2>
-      <p>{lead}</p>
+      <h1 className="name">{name}</h1>
+      <h2 className="title">{title}</h2>
+      <p className="lead">{lead}</p>
     </Tag>
   );
 };
@@ -31,4 +33,20 @@ ResumeHeader.defaultProps = {
 
 ResumeHeader.displayName = 'Resume.Header';
 
-export default styled(ResumeHeader)``;
+export default styled(ResumeHeader)`
+  .name {
+    ${type(TYPE_SIZE.t3)};
+    margin-bottom: 0;
+  }
+
+  .title {
+    ${type(TYPE_SIZE.t5)};
+    margin-top: 0;
+    margin-bottom: ${scaleToRem(0.25)};
+    color: ${COLORS.highlight};
+  }
+
+  .lead {
+    margin-top: 0;
+  }
+`;
