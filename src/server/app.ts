3d3c4242c83d1f9136ba05e4b6e83cd0a4be46fc
Eric Masiello <eric.j.masiello@gmail.com>
2018-04-23 08:49:17 -0400

    Disable x-powered-by and enable 2 week caching of static assets

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,10 +3,18 @@ import apiRouter from './routes/api';
 import uiRouter from './routes/ui';
 import errorMiddleware from './middleware/errorMiddleware';
 
+const TWO_WEEKS = 1000 * 60 * 60 * 24 * 14;
+
 const app = express();
+app.disable('x-powered-by'); // removes 'X-Powered-By: Express' header
 
 app.set('port', process.env.PORT || 3000);
-app.use(express.static('public'));
+
+app.use(
+  express.static('public', {
+    maxAge: TWO_WEEKS,
+  }),
+);
 app.set('view engine', 'ejs');
 
 app.use('/api', apiRouter);
