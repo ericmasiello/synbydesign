6256f104d4c646cd9414f21d50a428de3a4cef7d
Eric Masiello <eric.j.masiello@gmail.com>
2018-05-02 09:21:27 -0400

    Add comment with possible way to refactor how this works so these things are more composable

--- src/client/utils/swCache.ts
+++ src/client/utils/swCache.ts
@@ -3,6 +3,17 @@ export const CACHE_PREFIX = 'syn-cache-v';
 export const composeCacheName = (version: number) =>
   `${CACHE_PREFIX}${version}`;
 
+// FIXME: this name isn't really descriptive of what's going on.
+// technically this is respond with cache, falling back to network
+// fallbacking to default cached response
+//
+// Possible way to make this composable:
+/*
+  respondFromCache(event)
+    .then(respondeFromNetwork(event))
+    .then(cacheNetworkResponse)
+    .catch(fallbackToCachedResponse(OFFLINE_IMAGE_PLACEHOLDER))
+*/
 export const fallbackFromCache = (activeCacheName: string) => (
   event: ServiceWorkerEvent,
   cachedFallbackResponsePath: string,
