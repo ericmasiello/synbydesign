b34ccad347242038ead9fb9ac612d9ba6641a526
Eric Masiello <eric.j.masiello@gmail.com>
2018-05-02 09:08:29 -0400

    Update SW to cache images loaded from cloudfront and respond with the offline.svg if the image isn't cached

--- src/client/sw.ts
+++ src/client/sw.ts
@@ -1,14 +1,15 @@
 /// <reference path="../serviceworker.d.ts" />
-import { composeCacheName, CACHE_PREFIX } from './utils/swCache';
+import {
+  composeCacheName,
+  fallbackFromCache,
+  deleteInactiveCaches,
+} from './utils/swCache';
 import { FONT_URL } from './styles/vars';
 
 const CACHE_VERSION = 1;
 const CACHE_NAME = composeCacheName(CACHE_VERSION);
-
-interface ServiceWorkerEvent extends ExtendableEvent {
-  request: Request;
-  respondWith: (arg: Promise<any>) => void;
-}
+const OFFLINE_IMAGE_PLACEHOLDER = '/offline.svg';
+const fallback = fallbackFromCache(CACHE_NAME);
 
 const WEBPACK_BUNDLES: string[] = [
   // serviceWorkerOption is made available globally
@@ -16,6 +17,7 @@ const WEBPACK_BUNDLES: string[] = [
   // @ts-ignore
   ...serviceWorkerOption.assets,
   FONT_URL,
+  OFFLINE_IMAGE_PLACEHOLDER,
   '/', // store the home page
 ];
 
@@ -28,55 +30,52 @@ self.addEventListener('install', (e: Event) => {
   );
 });
 
-// TODO: implement the fallback to default image pattern for images that cannot be loaded if you are offline
-// this may require creating an offline image or svg that can be loaded in place of images
-// might want to use the offline.svg that exists in OfflineIcon.tsx
-
 self.addEventListener('fetch', (e: Event) => {
   const event = e as ServiceWorkerEvent;
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      console.log('-------------');
-      console.log('failing to fetch', event.request);
-      return caches.match(event.request).then(response => {
-        console.log(
-          'was response found in cache for',
-          event.request,
-          '? response = ',
-          response,
-        );
-        if (response) {
-          return response;
-        }
-        console.log(
-          'headers for',
-          event.request,
+  const requestURL = new URL(event.request.url);
+  if (requestURL.href.includes('cloudfront')) {
+    event.respondWith(fallback(event, OFFLINE_IMAGE_PLACEHOLDER));
+  } else {
+    // TODO: possibly make this handling more specific to actual pages using the
+    // pattern of inspecting the requestURL.pathname or href and seeing if they match
+    // '/' or '/portfolio/*'
+    // It might be wise to locally cache '/' but then create a fallback template for
+    // handling all other pages that do not include the server rendered React - this
+    // will avoid the client side error that the server generated html and client side
+    // React don't match
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        console.log('-------------');
+        console.log('failing to fetch', event.request);
+        return caches.match(event.request).then(response => {
+          console.log(
+            'was response found in cache for',
+            event.request,
+            '? response = ',
+            response,
+          );
+          if (response) {
+            return response;
+          }
+          console.log(
+            'headers for',
+            event.request,
+            // @ts-ignore
+            event.request.headers.get('accept').includes('text/html'),
+          );
           // @ts-ignore
-          event.request.headers.get('accept').includes('text/html'),
-        );
-        // @ts-ignore
-        if (event.request.headers.get('accept').includes('text/html')) {
-          const offline = caches.match('/');
-          console.log('returning offline', offline);
-          return offline;
-        }
-      });
-    }),
-  );
+          if (event.request.headers.get('accept').includes('text/html')) {
+            const offline = caches.match('/');
+            console.log('returning offline', offline);
+            return offline;
+          }
+        });
+      }),
+    );
+  }
 });
 
 self.addEventListener('activate', (e: Event) => {
   const event = e as ServiceWorkerEvent;
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (CACHE_NAME !== cacheName && cacheName.startsWith(CACHE_PREFIX)) {
-            return caches.delete(cacheName);
-          }
-          return false;
-        }),
-      );
-    }),
-  );
+  event.waitUntil(deleteInactiveCaches(CACHE_NAME));
 });
--- src/client/utils/swCache.ts
+++ src/client/utils/swCache.ts
@@ -2,3 +2,40 @@ export const CACHE_PREFIX = 'syn-cache-v';
 
 export const composeCacheName = (version: number) =>
   `${CACHE_PREFIX}${version}`;
+
+export const fallbackFromCache = (activeCacheName: string) => (
+  event: ServiceWorkerEvent,
+  cachedFallbackResponsePath: string,
+) => {
+  return caches.open(activeCacheName).then(cache => {
+    return cache.match(event.request).then(cacheResponse => {
+      return (
+        cacheResponse ||
+        fetch(event.request)
+          .then(networkResponse => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          .catch(() => {
+            return cache.match(cachedFallbackResponsePath);
+          })
+      );
+    });
+  });
+};
+
+export const deleteInactiveCaches = (activeCacheName: string) => {
+  return caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames.map(cacheName => {
+        if (
+          activeCacheName !== cacheName &&
+          cacheName.startsWith(CACHE_PREFIX)
+        ) {
+          return caches.delete(cacheName);
+        }
+        return false;
+      }),
+    );
+  });
+};
