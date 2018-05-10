73bccaf4f95dc5243fa394b89b81279bcf9b889b
Eric Masiello <eric.j.masiello@gmail.com>
2018-05-10 08:41:09 -0400

    Expand PortfolioDetailSVG view to display a primary SVG and 3 secondary SVGs colored differently

--- src/client/components/Portfolio/PortfolioDetailSVG.tsx
+++ src/client/components/Portfolio/PortfolioDetailSVG.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import * as tinyColor from 'tinycolor2';
+import PortfolioItemSVG from './PortfolioItemSVG';
 import { pxToRem } from '../../styles/utils';
 import { COLORS } from '../../styles/vars';
 
-interface Props {
+interface Props extends React.HTMLProps<HTMLElement> {
   className?: string;
   svgSource: string;
   tag?: Tag;
@@ -13,15 +15,27 @@ interface DefaultProps {
   tag: Tag;
 }
 
+const secondaryList = Array.from({ length: 3 }, (v, i) => i);
+
 export const PortfolioDetailSVG: React.SFC<Props> = props => {
   const { tag: Tag, className, svgSource, ...rest } = props as Props &
     DefaultProps;
   return (
-    <Tag
-      className={className}
-      dangerouslySetInnerHTML={{ __html: svgSource }}
-      {...rest}
-    />
+    <Tag className={className} {...rest}>
+      <PortfolioItemSVG
+        svgSource={svgSource}
+        className="portfolio-detail__svg portfolio-detail__svg--primary"
+      />
+      <div className="portfolio-detail__secondary">
+        {secondaryList.map(item => (
+          <PortfolioItemSVG
+            key={item}
+            svgSource={svgSource}
+            className="portfolio-detail__svg portfolio-detail__svg--secondary"
+          />
+        ))}
+      </div>
+    </Tag>
   );
 };
 
@@ -35,12 +49,35 @@ const StyledPortfolioDetailSVG = styled(PortfolioDetailSVG)`
   text-align: center;
   margin-top: ${pxToRem(50)};
 
-  svg {
+  .portfolio-detail__secondary {
+    max-width: ${pxToRem(960)};
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(${pxToRem(320)}, 1fr));
+  }
+
+  .portfolio-detail__svg {
     fill: ${COLORS.base};
     width: 100%;
     max-width: ${pxToRem(960)};
-    max-height: 80vh;
+    margin: auto;
+    padding: 1vw;
+
+    svg {
+      max-height: 80vh;
+    }
   }
+
+  ${secondaryList.map(
+    i =>
+      `
+      .portfolio-detail__svg--secondary:nth-child(${i + 1}) {
+        fill: ${tinyColor(COLORS.highlight)
+          .spin(90 * i)
+          .toString()};
+      }
+    `,
+  )};
 `;
 
 export default StyledPortfolioDetailSVG;
