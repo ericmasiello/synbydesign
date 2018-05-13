8d331d965729380a144aa4dc1bdf63aefa58a494
Eric Masiello <eric.j.masiello@gmail.com>
2018-05-13 15:42:26 -0400

    Hide nav except on home page

--- src/client/components/HeaderOnline.tsx
+++ src/client/components/HeaderOnline.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import * as H from 'history';
+import { withRouter, match } from 'react-router';
 import Header from './Header';
 import Logo from './Logo';
 import Nav from './Nav';
 
-export const HeaderOnline: React.SFC<{}> = () => {
+interface Props {
+  match: match<any>;
+  location: H.Location;
+  history: H.History;
+}
+
+export const HeaderOnline: React.SFC<Props> = props => {
   return (
     <Header>
       <Link to="/">
         <Logo />
       </Link>
-      <Nav />
+      {props.location.pathname === '/' ? <Nav /> : null}
     </Header>
   );
 };
 
 Header.displayName = 'HeaderOnline';
 
-export default HeaderOnline;
+export default withRouter(HeaderOnline);
