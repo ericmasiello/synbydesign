2ee845c12db278af427007ce78d76a4fdaeabaf4
Eric Masiello <eric.j.masiello@gmail.com>
2018-12-27 19:50:32 -0500

    Add Sentry error logging to server side

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,13 +1,19 @@
 import * as express from 'express';
 import * as compression from 'compression';
+import * as Sentry from '@sentry/node';
 import apiRouter from './routes/api';
 import uiRouter from './routes/ui';
 import errorMiddleware from './middleware/errorMiddleware';
 
 const TWO_WEEKS = 1000 * 60 * 60 * 24 * 14;
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
+
 const app = express();
 app.disable('x-powered-by'); // removes 'X-Powered-By: Express' header
+app.use(Sentry.Handlers.requestHandler());
 app.use(compression()); // enable gzip compression
 app.set('port', process.env.PORT || 3000);
 app.use(
@@ -19,6 +25,7 @@ app.set('view engine', 'ejs');
 
 app.use('/api', apiRouter);
 app.use('*', uiRouter);
+app.use(Sentry.Handlers.errorHandler());
 app.use(errorMiddleware);
 
 export default app;
