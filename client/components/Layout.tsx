4ce9647b25a0a238ce111898c7b63c71c8e9127b
Eric Masiello <eric.j.masiello@gmail.com>
2019-07-28 07:36:06 -0400

    Add image minification and error boundry

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import ErrorBoundry from './ErrorBoundry';
+import ErrorScreen from './ErrorScreen';
 import '../styles/base.scss';
 
 type Props = {
@@ -12,36 +14,46 @@ const Layout: React.FunctionComponent<Props> = ({
   title = 'This is the default title',
 }) => (
   <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/list-class">
-          <a>List Example</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/list-fc">
-          <a>List Example (as Functional Component)</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
+    <ErrorBoundry
+      render={({ resetError }) => <ErrorScreen resetError={resetError} />}
+    >
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com/"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/list-class">
+            <a>List Example</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/list-fc">
+            <a>List Example (as Functional Component)</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{' '}
+          |{' '}
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </ErrorBoundry>
   </div>
 );
 
--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,15 @@ const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const { env: prodEnv } = require('./now.json');
 
+const imageMinPlugin = new ImageminPlugin({
+  test: /\.(jpe?g|png|gif|svg)$/i,
+});
+
 const env = process.env.NODE_ENV === 'production' ? prodEnv : localEnv;
+const envPlugin = new webpack.EnvironmentPlugin(env);
 
 module.exports = withTypescript(
   withSass(
@@ -22,7 +28,8 @@ module.exports = withTypescript(
           loader: 'svg-inline-loader',
         });
 
-        config.plugins.push(new webpack.EnvironmentPlugin(env));
+        config.plugins.push(envPlugin);
+        config.plugins.push(imageMinPlugin);
 
         // Do not run type checking twice:
         if (options.isServer)
