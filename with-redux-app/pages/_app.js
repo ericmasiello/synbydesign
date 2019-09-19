19458ec260cb33771735a7365682fbdd33f0daf0
Eric Masiello <eric.j.masiello@gmail.com>
2019-09-19 08:12:59 -0400

    Improve network connection/dns work by adding preconnect for other domains

--- with-redux-app/pages/_app.js
+++ with-redux-app/pages/_app.js
@@ -1,17 +1,22 @@
-import App from 'next/app'
-import React from 'react'
-import withReduxStore from '../lib/with-redux-store'
-import { Provider } from 'react-redux'
+import App from 'next/app';
+import React from 'react';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import withReduxStore from '../lib/with-redux-store';
 
 class MyApp extends App {
-  render () {
-    const { Component, pageProps, reduxStore } = this.props
+  render() {
+    const { Component, pageProps, reduxStore } = this.props;
     return (
       <Provider store={reduxStore}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://res.cloudinary.com" />
+        </Head>
         <Component {...pageProps} />
       </Provider>
-    )
+    );
   }
 }
 
-export default withReduxStore(MyApp)
+export default withReduxStore(MyApp);
