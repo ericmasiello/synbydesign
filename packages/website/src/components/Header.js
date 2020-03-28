282121330d5cd30d2c13cb93f2b31cef072fd2af
Eric Masiello <eric.j.masiello@gmail.com>
2020-03-28 18:42:02 -0400

    Move x on scroll

--- packages/website/src/components/Header.js
+++ packages/website/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef, useCallback } from 'react';
 import classNames from 'classnames';
-import { Link } from "gatsby"
+import { Link } from 'gatsby';
 import AnimatedLink from './AnimatedLink';
 import { Nav, NavList, NavItem } from './Nav';
 import Logo from './Logo';
@@ -10,28 +10,65 @@ import styles from './Header.module.css';
 
 function Header(props) {
   const { className, ...rest } = props;
+
+  const xRef = useRef();
+
+  const handleScroll = useCallback(() => {
+    if (!xRef.current) {
+      return;
+    }
+    requestAnimationFrame(() => {
+      const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+      const amount = `${scroll / 100}%`;
+      const invert = `${scroll / 100}%`;
+      document.body.style.setProperty('--scroll-amount', amount);
+      document.body.style.setProperty('--scroll-invert', invert);
+    });
+  }, [xRef]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const classes = classNames(styles.header, className);
   return (
     <header className={classes} {...rest}>
       <Link to="/">
-        <Logo
-          as="h1"
-          className={styles.logo}
-        >
+        <Logo as="h1" className={styles.logo}>
           Syn By Design
         </Logo>
       </Link>
-      <Text as="p">Freelance <span className={styles.role}>developer</span>, <span className={styles.role}>trainer</span>, & <span className={styles.role}>consultant</span>.</Text>
+      <Text as="p">
+        Freelance <span className={styles.role}>developer</span>, <span className={styles.role}>trainer</span>, &{' '}
+        <span className={styles.role}>consultant</span>.
+      </Text>
       <Nav>
         <NavList>
-          <NavItem><AnimatedLink as="a" href="#gallery">Selected Work</AnimatedLink></NavItem>
-          <NavItem><AnimatedLink as="a" href="#resume">Resume</AnimatedLink></NavItem>
-          <NavItem><AnimatedLink as="a" href="https://medium.com/@EricMasiello">Blog</AnimatedLink></NavItem>
+          <NavItem>
+            <AnimatedLink as="a" href="#gallery">
+              Selected Work
+            </AnimatedLink>
+          </NavItem>
+          <NavItem>
+            <AnimatedLink as="a" href="#resume">
+              Resume
+            </AnimatedLink>
+          </NavItem>
+          <NavItem>
+            <AnimatedLink as="a" href="https://medium.com/@EricMasiello">
+              Blog
+            </AnimatedLink>
+          </NavItem>
         </NavList>
       </Nav>
-      <X className={styles.x} />
+      <div ref={xRef} className={styles.x}>
+        <X />
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
