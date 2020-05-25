aa91dd43e66b98ac5ac807c580ade655c7c964c6
Eric Masiello <eric.j.masiello@gmail.com>
2020-05-25 11:38:42 -0400

    remove any existing service workers

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -56,6 +56,7 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-remove-serviceworker`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
