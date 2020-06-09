7766fe9f27aea7844c66cf89c4adf9806904c66f
Eric Masiello <eric.j.masiello@gmail.com>
2020-06-09 06:46:17 -0400

    experiment with loading fonts async

--- packages/website/gatsby-browser.js
+++ packages/website/gatsby-browser.js
@@ -2,3 +2,10 @@ import './src/styles/global.css';
 import GatsbyPageWrapper from './src/components/GatsbyPageWrapper';
 
 export const wrapPageElement = GatsbyPageWrapper;
+
+export const onInitialClientRender = () => {
+  document.querySelector('#google-font-link').onload = function () {
+    // change to 'all' so that fonts are applied to page
+    this.media = 'all';
+  };
+};
--- packages/website/src/components/layout.js
+++ packages/website/src/components/layout.js
@@ -4,14 +4,15 @@ import { Helmet } from 'react-helmet';
 import '@synbydesign/common-ui/dist/index.css';
 import styles from './Layout.module.css';
 
+const FONTS = 'https://fonts.googleapis.com/css?family=Lato:400,700|Source+Sans+Pro:300,400,600&display=swap';
+
 const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Lato:400,700|Source+Sans+Pro:300,400,600&display=swap"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preload" href={FONTS} as="style" />
+        <link rel="stylesheet" href={FONTS} media="print" id="google-font-link" />
       </Helmet>
       <main className={styles.content}>{children}</main>
     </>
