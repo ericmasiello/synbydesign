5ee2f9b34362ad6caae591c8de774c1b538172f0
Eric Masiello <eric.j.masiello@gmail.com>
2020-06-28 14:57:30 -0400

    separate critcal fonts from non-critical fonts

--- packages/website/src/components/layout.js
+++ packages/website/src/components/layout.js
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import '@synbydesign/common-ui/dist/index.css';
 import styles from './Layout.module.css';
 
+const FONT_CRITICAL = 'https://fonts.googleapis.com/css?Source+Sans+Pro:300,600&display=swap';
 const FONTS = 'https://fonts.googleapis.com/css?family=Lato:400,700|Source+Sans+Pro:300,400,600&display=swap';
 
 const Layout = ({ children }) => {
@@ -11,14 +12,15 @@ const Layout = ({ children }) => {
     <>
       <Helmet>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link rel="preload" href={FONTS} as="style" />
+        <link rel="preload" href={FONT_CRITICAL} as="style" />
         {/*
-          NOTE: the below <link /> automatically has the following two attributes added to it
+          NOTE: the below <link />s automatically has the following two attributes added to it
           at build time via netlify-plugin-inline-critical-css
           * media="print"
           * onload="this.meida='all'"
           The plugin is configured via Netlify's plugins page https://app.netlify.com/teams/ericmasiello/plugins
         */}
+        <link rel="stylesheet" href={FONT_CRITICAL} />
         <link rel="stylesheet" href={FONTS} />
       </Helmet>
       <main className={styles.content}>{children}</main>
