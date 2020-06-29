f150c5f009d34fd6b66074d659c3f3401a69c32b
Eric Masiello <eric.j.masiello@gmail.com>
2020-06-29 07:48:58 -0400

    add offline support

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -56,9 +56,9 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-remove-serviceworker`,
+    // `gatsby-plugin-remove-serviceworker`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
