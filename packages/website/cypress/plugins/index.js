fbf8bc16ce490f3a9e5f6046ca5d186d2396acc9
Eric Masiello <eric.j.masiello@gmail.com>
2021-03-21 19:39:03 -0400

    fix: build

--- packages/website/cypress/plugins/index.js
+++ packages/website/cypress/plugins/index.js
@@ -20,3 +20,11 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
+
+module.exports = (on, config) => {
+  /** the rest of your plugins... **/
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  require('cypress-log-to-output').install(on);
+  // or, if there is already a before:browser:launch handler, use .browserLaunchHandler inside of it
+  // @see https://github.com/flotwig/cypress-log-to-output/issues/5
+};
