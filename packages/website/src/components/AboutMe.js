5cd745d1a49617377ecd50c9011fe48fb7dbe834
Eric Masiello <emasiello@vistaprint.com>
2022-01-17 20:40:06 -0500

    Add about me

---
+++ packages/website/src/components/AboutMe.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './AboutMe.module.css';
+
+function AboutMe(props) {
+  const { as: Component, content, className, ...rest } = props;
+  const classes = classNames(styles.aboutMe, className);
+  return (
+    <Component className={classes} {...rest}>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </Component>
+  );
+}
+
+AboutMe.defaultProps = {
+  as: 'div',
+  content: '',
+};
+
+AboutMe.propTypes = {
+  as: PropTypes.elementType,
+  content: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default AboutMe;
