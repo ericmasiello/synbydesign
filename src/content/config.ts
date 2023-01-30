3086a97d73e890564ca0dfc63821617b6002f32d
Eric Masiello <emasiello@vistaprint.com>
2023-01-30 09:49:47 -0500

    refactor: talks to content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,9 +26,19 @@ const professionalExperienceCollection = defineCollection({
   }),
 });
 
+const talksCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    url: z.string().optional(),
+    year: z.number(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   education: educationCollection,
   professionalExperience: professionalExperienceCollection,
+  talks: talksCollection,
 };
---
+++ src/content/talks/accessibleComponents.md
@@ -0,0 +1,6 @@
+---
+
+description: "At Vistaprint, we believe accessibility is vital. This talk showcases a tabbed interface. I explain why it is inaccessible and then walk through how to refactor the code to maintain the same visual presentation but make it fully accessible to keyboard and screen reader users using ARIA roles, labels, and JavaScript."
+title: "Let's Make an Accessible Component"
+url: "https://slides.com/ericmasiello/accessible-component"
+year: 2018
